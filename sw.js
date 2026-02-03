46e08971b5b03eb6bbc62d08b060d04ac434840e
woodori1234 <woodori1234@gmail.com>
2026-02-04 01:01:33 +0900

    Add service worker for offline support

---
+++ sw.js
@@ -0,0 +1,42 @@
+// 디지털 디톡스 타이머 - Service Worker
+const CACHE_NAME = 'detox-timer-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/app.js',
+    '/manifest.json',
+    '/icon-192.svg',
+    '/icon-512.svg'
+];
+
+// 설치 시 캐시
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// 요청 시 캐시 우선
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
+
+// 활성화 시 오래된 캐시 삭제
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
